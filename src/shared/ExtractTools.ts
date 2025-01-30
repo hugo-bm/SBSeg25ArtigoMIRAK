c24da7f099c979993a371a71c2a64d4bc09b76b5
Hugo Batalha Moreno <hugomb123@gmail.com>
2025-01-30 01:38:55 -0300

    ✨ feat(src/shared): added functionality to extract the CPE version
    Extract the CPE version so that it can be numerically comparable. To do this, it was established that each value between the points will be an item added to a return list, with any non-numeric value being ignored and any numeric value separated from the points being added to the last value in the list. Finally, if there is only the identifier of any “*”, a list containing the largest possible integer value is returned.

--- src/shared/ExtractTools.ts
+++ src/shared/ExtractTools.ts
@@ -4,4 +4,26 @@ export default class ExtractTools{
         // Converts matches to numbers and returns the array
         return matches ? matches.map(Number) : null;
       }
+      public static extractVersionProduct(cpe: string): number[] {
+        const versionField = cpe.split(":")[5];
+        if (new RegExp("^d+(.d+)*$").test(versionField)) {
+          return versionField.split(".").map((num) => parseInt(num));
+        } else {
+          if (versionField === "*") {
+            return [Number.MAX_SAFE_INTEGER];
+          }
+          return versionField.split(".").map((part) => {
+            if (isNaN(parseInt(part))) {
+              return parseInt(part);
+            } else {
+              const numbers = ExtractTools.extractNumbersFromString(part);
+              if (numbers == null) {
+                return 0;
+              } else {
+                return numbers.reduce((prev, curr) => prev + curr);
+              }
+            }
+          });
+        }
+      }
 }
